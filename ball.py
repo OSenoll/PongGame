6a9eb71740e139bed0f5070c9fd42b127a4e8634
omer <omersenol30@gmail.com>
2023-09-10 23:44:21 +0300

    ball movement

---
+++ ball.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.goto(0, 0)
+
+    def move(self):
+        new_x = self.xcor() + 1
+        new_y = self.ycor() + 1
+        self.goto(new_x, new_y)
+
