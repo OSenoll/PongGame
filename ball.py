f15252ffd3d6fd9d00e5df2a15d194e57b5f3d61
omer <omersenol30@gmail.com>
2023-09-11 19:16:25 +0300

    finishing scoreboard

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
@@ -7,9 +8,23 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.goto(0, 0)
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+    def reset_position(self):
+        self.goto(0, 0)
+        self.bounce_x()
+        self.move_speed = 0.1
 
